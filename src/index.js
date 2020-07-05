2416bdcc9c71a88084cd2c53b68ecac773aca489
name <wilson@DESKTOP-M2E7CGM.localdomain>
2020-07-05 12:33:10 -0500

    ESlint

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {render} from "react-dom";
+import HelloWorld from "./components/HelloWorld"
+import "./assets/styles/App.scss"
+render(
+    <HelloWorld />,
+    document.getElementById("app")
+);
+
+
